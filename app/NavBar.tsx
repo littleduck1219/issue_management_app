5c371e18c309a9a41efb4641b39d35ec93414572
gyeong deok <littleduck1219@gmail.com>
2024-03-09 08:37:24 +0900

    [Design]navigation bar

---
+++ app/NavBar.tsx
@@ -0,0 +1,30 @@
+import Link from "next/link";
+import React from "react";
+import { FaTasks } from "react-icons/fa";
+
+const NavBar = () => {
+    const links = [
+        { label: "Dashboard", href: "/" },
+        { label: "Issues", href: "/issues" },
+    ];
+
+    return (
+        <nav className='flex space-x-6 border-b mb-5 px-5 items-center h-14'>
+            <Link href='/'>
+                <FaTasks />
+            </Link>
+            <ul className='flex space-x-6'>
+                {links.map((link) => (
+                    <Link
+                        className='text-zinc-500 hover:text-zinc-800 transition-colors'
+                        key={link.href}
+                        href={link.href}>
+                        {link.label}
+                    </Link>
+                ))}
+            </ul>
+        </nav>
+    );
+};
+
+export default NavBar;
