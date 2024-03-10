08349ecb58b7ea8786cab2f23718f5c8b527e4ab
gyeong deok <littleduck1219@gmail.com>
2024-03-11 00:56:23 +0900

    [Feat]texteditor simplemde

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,7 +1,9 @@
 "use client";
 
-import { Button, TextField, TextArea } from "@radix-ui/themes";
+import { Button, TextField } from "@radix-ui/themes";
 import React from "react";
+import SimpleMDE from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
 
 export default function page() {
     return (
@@ -9,7 +11,7 @@ export default function page() {
             <TextField.Root>
                 <TextField.Input placeholder='Title' />
             </TextField.Root>
-            <TextArea placeholder='Description' />
+            <SimpleMDE placeholder='Description' />
             <Button>Create New Issue</Button>
         </div>
     );
