e6a86acbaa0149b95b69ac8e4850b13f14f47bc7
gyeong deok <littleduck1219@gmail.com>
2024-03-18 22:31:51 +0900

    [Fix]issue data

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,23 @@
 import prisma from "@/prisma/client";
+import { Issue } from "@prisma/client";
 import IssueSummary from "./IssueSummary";
 import IssueChart from "./IssueChart";
 import { Flex, Grid } from "@radix-ui/themes";
 import LatestIssues from "./LatestIssues";
 import { Metadata } from "next";
+import { useEffect, useState } from "react";
 
 export default async function Home({ searchParams }: { searchParams: { page: string } }) {
-    const issuesData = await prisma.issue.findMany();
+    const [issuesData, setIssuesData] = useState<Issue[]>([]);
+
+    useEffect(() => {
+        const fetchIssuesData = async () => {
+            const data = await prisma.issue.findMany();
+            setIssuesData(data);
+        };
+        fetchIssuesData();
+    }, [searchParams]);
+
     const open = issuesData.filter((issue) => issue.status === "OPEN").length;
     const inProgress = issuesData.filter((issue) => issue.status === "IN_PROGRESS").length;
     const closed = issuesData.filter((issue) => issue.status === "CLOSED").length;
@@ -21,6 +32,8 @@ export default async function Home({ searchParams }: { searchParams: { page: str
         </Grid>
     );
 }
+
+export const dynamic = "force-dynamic";
 
 export const metadata: Metadata = {
     title: "Work Manager - Dashboard",
