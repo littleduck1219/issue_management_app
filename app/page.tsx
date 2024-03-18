726bc3b25a4722e8e2d0d6bda0a4bb5d5d4a416b
gyeong deok <littleduck1219@gmail.com>
2024-03-18 22:42:46 +0900

    [Fix]issue count

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,14 @@
-"use client";
-
 import prisma from "@/prisma/client";
-import { Issue } from "@prisma/client";
 import IssueSummary from "./IssueSummary";
 import IssueChart from "./IssueChart";
 import { Flex, Grid } from "@radix-ui/themes";
 import LatestIssues from "./LatestIssues";
 import { Metadata } from "next";
-import { useEffect, useState } from "react";
 
-export default function Home({ searchParams }: { searchParams: { page: string } }) {
-    const [issuesData, setIssuesData] = useState<Issue[]>([]);
-
-    useEffect(() => {
-        const fetchIssuesData = async () => {
-            const data = await prisma.issue.findMany();
-            setIssuesData(data);
-        };
-        fetchIssuesData();
-    }, [searchParams]);
-
-    const open = issuesData.filter((issue) => issue.status === "OPEN").length;
-    const inProgress = issuesData.filter((issue) => issue.status === "IN_PROGRESS").length;
-    const closed = issuesData.filter((issue) => issue.status === "CLOSED").length;
+export default async function Home({ searchParams }: { searchParams: { page: string } }) {
+    const open = await prisma.issue.count({ where: { status: "OPEN" } });
+    const inProgress = await prisma.issue.count({ where: { status: "IN_PROGRESS" } });
+    const closed = await prisma.issue.count({ where: { status: "CLOSED" } });
 
     return (
         <Grid columns={{ initial: "1", md: "2" }} gap='5'>
@@ -35,9 +21,7 @@ export default function Home({ searchParams }: { searchParams: { page: string } 
     );
 }
 
-export const dynamic = "force-dynamic";
-
-// export const metadata: Metadata = {
-//     title: "Work Manager - Dashboard",
-//     description: "Can check work status and latest issues.",
-// };
+export const metadata: Metadata = {
+    title: "Work Manager - Dashboard",
+    description: "Can check work status and latest issues.",
+};
