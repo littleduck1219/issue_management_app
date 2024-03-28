e4691cf0e670d584dc95e8906763eccde06f7387
gyeong deok <littleduck1219@gmail.com>
2024-03-29 03:46:03 +0900

    [Fix]fix login page

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import LatestIssues from "./_components/LatestIssues";
 import { Metadata } from "next";
 import { authOptions } from "./auth/authOption";
 
-export default async function Home({ searchParams }: { searchParams: { page: string } }) {
+export default async function Home({ searchParams }: { searchParams?: { page: string } }) {
     const open = await prisma.issue.count({ where: { status: "OPEN" } });
     const inProgress = await prisma.issue.count({ where: { status: "IN_PROGRESS" } });
     const closed = await prisma.issue.count({ where: { status: "CLOSED" } });
